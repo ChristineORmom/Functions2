9503524df9f37c7dcd8b5571ead31ab4e7855f66
Christine VanDehey <chrisdalvan@gmail.com>
2022-03-09 14:06:33 -0800

    post tax prices

--- arrayMethods.js
+++ arrayMethods.js
@@ -44,7 +44,14 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices  = prices.map(/* Provide Your Callback Here );
+const postTaxPrices  = prices.map(function(element) {
+  retrun element * 1.07
+})
+
+// with arrow function
+// const postTaxPrices = prices.map(num => num * 1.07)
+
+console.log(postTaxPrices)
 
 
 
